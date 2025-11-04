d1024244f743ced762e99875ddee0e6a98d063f0
DarkhanOmirbay <domirbai04@gmail.com>
2025-11-04 17:41:12 +0500

    upd crud : user's department

--- app/crud/user.py
+++ app/crud/user.py
@@ -13,3 +13,16 @@ async def read_user(session: AsyncSession, user_id: int):
 async def create_user(session: AsyncSession, user: User):
     session.add(user)
     await session.commit()
+
+
+async def update_user_department(session: AsyncSession, user_id: int, department: str):
+    query = select(User).where(User.id == user_id)
+    result = await session.execute(query)
+    user = result.scalar_one_or_none()
+
+    if user:
+        user.department = department
+        await session.commit()
+        await session.refresh(user)
+        return user
+    return None
