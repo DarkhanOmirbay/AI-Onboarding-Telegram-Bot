1f11a39d44a9593977dc3007ef39c988efd410a3
DarkhanOmirbay <domirbai04@gmail.com>
2025-11-04 17:41:31 +0500

    add department keyboard

--- app/keyboards/keyboards.py
+++ app/keyboards/keyboards.py
@@ -3,3 +3,15 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 stop = InlineKeyboardMarkup(
     inline_keyboard=[[InlineKeyboardButton(text="STOP Q&A", callback_data="stop_qa")]]
 )
+department_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Development team(DEV)", callback_data="dep_dev")],
+        [InlineKeyboardButton(text="Product team(PROD)", callback_data="dep_prod")],
+        [InlineKeyboardButton(text="Customer care(CUSCARE)", callback_data="dep_cuscare")],
+        [InlineKeyboardButton(text="Финансовый отдел", callback_data="dep_fin")],
+        [InlineKeyboardButton(text="Government relations(GR)", callback_data="dep_gr")],
+        [InlineKeyboardButton(text="HR", callback_data="dep_hr")],
+        [InlineKeyboardButton(text="Коммерческий отдел", callback_data="dep_commercial")],
+        [InlineKeyboardButton(text="Маркетинг", callback_data="dep_marketing")],
+    ]
+)
